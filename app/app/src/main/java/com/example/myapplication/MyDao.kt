048a6d888eb71786e3bc2300b3d7d936a966ccf1
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-09-18 18:28:44 +0900

    Update MyDao.kt

--- app/app/src/main/java/com/example/myapplication/MyDao.kt
+++ app/app/src/main/java/com/example/myapplication/MyDao.kt
@@ -6,15 +6,15 @@ import androidx.room.*
 @Dao
 interface MyDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)  // INSERT, key 충돌이 나면 새 데이터로 교체
-    fun insertStudent(student: Student)
+    suspend fun insertStudent(student: Student)
 
     @Query("SELECT * FROM student_table")
     fun getAllStudents(): LiveData<List<Student>>        // LiveData<> 사용
 
     @Query("SELECT * FROM student_table WHERE name = :sname")   // 메소드 인자를 SQL문에서 :을 붙여 사용
-    fun getStudentByName(sname: String): List<Student>
+    suspend fun getStudentByName(sname: String): List<Student>
 
     @Delete
-    fun deleteStudent(student: Student); // primary key is used to find the student
+    suspend fun deleteStudent(student: Student); // primary key is used to find the student
 
 }
